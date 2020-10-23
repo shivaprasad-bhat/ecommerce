7fd08541dfd2b2a5e1e1bcae05012e0a85c11878
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-10-24 00:30:32 +0530

    integrated frontend to backend api call

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import products from '../products.js';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import axios from 'axios';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+            setProducts(data);
+        };
+
+        fetchProducts();
+    }, []);
     return (
         <>
             <h1>Latest Products</h1>
