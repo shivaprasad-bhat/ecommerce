ad6fb8d785e5d68c476d7fde4343be4bd0d495bd
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-10-24 16:13:48 +0530

    Adding models for the backend; User, Products, Order

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+import 'colors';
 
 dotenv.config();
-
+connectDB();
 const app = express();
-
-import products from './data/products.js';
 
 app.get('/', (req, res) => {
     res.send('Server Running');
@@ -22,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
     PORT,
     console.log(
-        `Server running on port ${PORT} in ${process.env.NODE_ENV} mode`
+        `Server running on port ${PORT} in ${process.env.NODE_ENV} mode`.green
+            .inverse
     )
 );
