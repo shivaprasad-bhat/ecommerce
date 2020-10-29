abaf59af54fef8c9f6685917cba48f6bb476a694
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-10-29 21:24:04 +0530

    Adding Update Profile route in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
     authUser,
     getUserProfile,
     registerUser,
+    updateProfile,
 } from '../controllers/userController.js';
 import protect from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.route('/').post(registerUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateProfile);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from './Screens/CartScreen';
 import ProductScreen from './Screens/ProductScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen';
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
             <main className="py-3">
                 <Container>
                     <Route path="/login" component={LoginScreen} />
+                    <Route path="/register" component={RegisterScreen} />
                     <Route path="/product/:id" component={ProductScreen} />
                     <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/" component={Home} exact />
