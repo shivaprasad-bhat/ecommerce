2438652afcbf31060ff689cbd3458452425483a8
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-11-05 22:59:54 +0530

    Adding files for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server Running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -34,8 +30,22 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve();
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        return res.sendFile(
+            path.resolve(__dirname, 'frontend/build/index.html')
+        );
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Server Running');
+    });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
