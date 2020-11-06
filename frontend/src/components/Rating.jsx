fe74840e02f65a1d5b1ccb7ee241c1dd4d555d4e
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-11-06 11:24:46 +0530

    Fixing Rating Starts bug. Displaying all 5 stars

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -11,9 +11,9 @@ const Rating = ({ value, text, color }) => {
                             color,
                         }}
                         className={
-                            value > 1
+                            value >= 1
                                 ? 'fas fa-star'
-                                : value > 0.5
+                                : value >= 0.5
                                 ? 'fas fa-start-half-alt'
                                 : 'far fa-star'
                         }
@@ -25,9 +25,9 @@ const Rating = ({ value, text, color }) => {
                             color,
                         }}
                         className={
-                            value > 2
+                            value >= 2
                                 ? 'fas fa-star'
-                                : value > 1.5
+                                : value >= 1.5
                                 ? 'fas fa-start-half-alt'
                                 : 'far fa-star'
                         }
@@ -39,9 +39,9 @@ const Rating = ({ value, text, color }) => {
                             color,
                         }}
                         className={
-                            value > 3
+                            value >= 3
                                 ? 'fas fa-star'
-                                : value > 2.5
+                                : value >= 2.5
                                 ? 'fas fa-start-half-alt'
                                 : 'far fa-star'
                         }
@@ -53,9 +53,9 @@ const Rating = ({ value, text, color }) => {
                             color,
                         }}
                         className={
-                            value > 4
+                            value >= 4
                                 ? 'fas fa-star'
-                                : value > 3.5
+                                : value >= 3.5
                                 ? 'fas fa-start-half-alt'
                                 : 'far fa-star'
                         }
@@ -67,9 +67,9 @@ const Rating = ({ value, text, color }) => {
                             color,
                         }}
                         className={
-                            value > 5
+                            value >= 5
                                 ? 'fas fa-star'
-                                : value > 4.5
+                                : value >= 4.5
                                 ? 'fas fa-start-half-alt'
                                 : 'far fa-star'
                         }
